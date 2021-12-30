886de4926cbbbff20cf446f0442f256cab9f6813
Vadim Dagman <vadim@toptal.com>
2021-12-30 07:16:33 -0800

    Initial Commit

---
+++ BlogCompanion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlogCompanion
+//
+//  Created by Vadim on 12/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
